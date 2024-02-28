4bafdede90b7113494041dca6a5aa4aa07c5f4c3
KaterunaR <131966803+KaterunaR@users.noreply.github.com>
2024-02-29 00:56:18 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public double DailyMaintenanceCost { get; set; }
+
+        public Animal(string name, double weight, int age, double dailyMaintenanceCost)
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
+            DailyMaintenanceCost = dailyMaintenanceCost;
+        }
+
+        public virtual string GetInfo()
+        {
+            return $"Name: {Name}, Weight: {Weight} kg, Age: {Age} years, Daily Maintenance Cost: ${DailyMaintenanceCost}";
+        }
+    }
+
+    public class Wolf : Animal
+    {
+        public string Breed { get; set; }
+        public string NaturalLocation { get; set; }
+
+        public Wolf(string name, double weight, int age, double dailyMaintenanceCost, string breed, string naturalLocation)
+            : base(name, weight, age, dailyMaintenanceCost)
+        {
+            Breed = breed;
+            NaturalLocation = naturalLocation;
+        }
+
+        public override string GetInfo()
+        {
+            return $"{base.GetInfo()}, Breed: {Breed}, Natural Location: {NaturalLocation}";
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using lab1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        private Animal fish;
+        private Wolf arcticWolf;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fish = new Animal("риба", 2, 5, 20);
+            arcticWolf = new Wolf("вовк", 40, 3, 15, "лісовий", "ліс");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DisplayAnimalInfo(fish);
+        }
+
+        private void DisplayAnimalInfo(Animal animal)
+        {
+            textBox1.Text = animal.Name;
+            textBox2.Text = animal.Weight.ToString();
+            textBox3.Text = animal.Age.ToString();
+            textBox4.Text = animal.DailyMaintenanceCost.ToString();
+
+            if (animal is Wolf wolf)
+            {
+                textBox5.Text = wolf.Breed;
+                textBox6.Text = wolf.NaturalLocation;
+            }
+            else
+            {
+                textBox5.Text = "";
+                textBox6.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DisplayAnimalInfo(arcticWolf);
+        }
+
+
+    }
+}
+
